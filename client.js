dabf68468acaea8bdcbf3b53fa3ffe1d74215a9d
Yash Jethmalani <yashjethmalani@gmail.com>
2019-07-29 20:30:47 +0530

    basic video player implentation

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import {ReactInstance, Surface} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -12,9 +12,8 @@ function init(bundle, parent, options = {}) {
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('take6', { 
+    r360.createRoot('VideoPlayer', { 
         
-
     }),
     r360.getDefaultSurface()
   );
--- index.js
+++ index.js
@@ -2,51 +2,136 @@ import React from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
   VrButton,
-  Image,
   Environment,
-  NativeModules,
+  asset,
+  Text
 } from 'react-360';
 import VideoModule from 'VideoModule';
 
- 
-export default class take6 extends React.Component {
-  fishVideo = () => {
- 
-  // Play fish video
-  VideoModule.createPlayer('myplayer');
-  VideoModule.play('myplayer', { source: {url: asset('video.mp4').uri}, muted: false });
-  Environment.setScreen('default', 'myplayer', 'default', 0, 0, 1000, 500);
-}; 
+const customPlayerStyles = StyleSheet.create({
+  playOrPause: {
+    //TODO: Add styles for the Play Or Pause button
+  },
+  progress: {
+    //TODO: Add styles for progress
+  },
+});
+
+class CustomVideoPlayer extends React.Component {
+  // Create a player
+  state = {
+    showPlay: true,
+    showPause: true,
+    videoEvent: {}
+  }
+
+  componentWillMount() {
+    this.player = VideoModule.createPlayer('customPlayer');
+  }
+
+  componentDidMount() {
+    this.playSource(this.props.video.source);
+    // Or, play in-line on a surface
+    Environment.setScreen(
+      'default', /* screen name */
+      'customPlayer', /* player unique id */
+      'default', /* surface name */
+      0, 0, 1000, 500 /* relative position on the surface */
+    );
+
+    this.player.addListener('onVideoStatusChanged', (event) => {
+      let updateObj = {
+        videoEvent: event
+      }
+
+      switch(event.status) {
+        case 'ready':
+          updateObj.showPlay = true
+          this.setState(updateObj)
+          break;
+        case 'paused':
+          updateObj.showPause = false
+          updateObj.showPlay = true
+          this.setState(updateObj)
+          break;
+        case 'playing':
+          updateObj.showPause = true
+          updateObj.showPlay = false
+          this.setState(updateObj)
+          break;
+        case 'finished':
+          updateObj.showPlay = true
+          updateObj.showPause = false
+          this.setState(updateObj)
+          break;
+        case 'stopped':
+          updateObj.showPlay = true
+          updateObj.showPause = false
+          this.setState(updateObj)
+          break;
+        case 'seeking':
+          updateObj.showPlay = true
+          updateObj.showPause = false
+        case 'failed':
+          //TODO: show retry
+          break;
+        case 'closing':
+          //TODO: show message that says session is closing
+          break;
+        case 'closed': 
+          //TODO: No session. Hide control overlay
+        default: break;
+      };
+
+      
+      // status - Video play status, can be
+      // closed: No session.
+      // closing: Session is being closed.
+      // failed: Session had an error.
+      // finished: Session has finished playing video
+      // : Session is seeking a position
+    })
+  }
+
+  componentWillUnmount() {
+    this.player.destroy()
+  }
+
+  playSource (source) {
+    this.player.play({
+      source: {url: asset(source).uri}, // provide the path to the video
+      stereo: '3DTB', // optionally, supply the format of the video
+      muted: false
+    });
+  }
+
   render() {
     return (
       <View>
-      {this.fishVideo()}
-    </View>
+          {this.state.showPlay ? <VrButton onClick={(e) => { this.player.resume() }}><Text>Resume</Text></VrButton> : <View/>}
+          {this.state.showPause ? <VrButton onClick={(e) => { this.player.pause() }}><Text>Pause</Text></VrButton> : <View/>}
+          <Text>{this.state.videoEvent.position || '-'}/{this.state.videoEvent.duration || '-'}</Text>
+          <Text>Volume: {this.state.videoEvent.volume || '-'}</Text>
+      </View>
+    )
+  }
+}
+
+export default class VideoPlayer extends React.Component {
+  render() {
+    return (
+      <View>
+        <CustomVideoPlayer 
+          video={{source: 'video.mp4'}} 
+        />
+      </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
 });
 
-AppRegistry.registerComponent('take6', () => take6);
+AppRegistry.registerComponent('VideoPlayer', () => VideoPlayer);
